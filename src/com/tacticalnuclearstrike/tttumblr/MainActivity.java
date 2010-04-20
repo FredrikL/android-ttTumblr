df77abcdd42c13f233512d5b9ede33c61e887bcb
Fredrik Leijon <fredrik.leijon@gmail.com>
2010-04-20 21:06:48 +0200

    removed unused import

--- src/com/tacticalnuclearstrike/tttumblr/MainActivity.java
+++ src/com/tacticalnuclearstrike/tttumblr/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.tacticalnuclearstrike.tttumblr.activites.PostConversationActivity;
