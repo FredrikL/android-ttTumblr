58e4cb6f604f0d81cfba5376344ec2cf67494eaf
Fredrik Leijon <fredrik.leijon@gmail.com>
2010-04-24 15:06:21 +0200

    removed some imports

--- src/com/tacticalnuclearstrike/tttumblr/activites/UploadVideoActivity.java
+++ src/com/tacticalnuclearstrike/tttumblr/activites/UploadVideoActivity.java
@@ -1,20 +1,16 @@
 package com.tacticalnuclearstrike.tttumblr.activites;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.tacticalnuclearstrike.tttumblr.R;
