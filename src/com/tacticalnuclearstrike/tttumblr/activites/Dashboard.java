4a5446031410aa6c2735e6830a5ec66d3ad3a364
Fredrik Leijon <fredrik.leijon@gmail.com>
2010-04-24 14:59:23 +0200

    enble javascript & stop links from opening in browser

--- src/com/tacticalnuclearstrike/tttumblr/activites/Dashboard.java
+++ src/com/tacticalnuclearstrike/tttumblr/activites/Dashboard.java
@@ -2,7 +2,6 @@ package com.tacticalnuclearstrike.tttumblr.activites;
 
 import java.util.List;
 
-import org.apache.http.Header;
 import org.apache.http.cookie.Cookie;
 
 import android.app.Activity;
@@ -10,6 +9,7 @@ import android.os.Bundle;
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.tacticalnuclearstrike.tttumblr.TumblrApi;
 
@@ -25,7 +25,6 @@ public class Dashboard extends Activity {
 		TumblrApi api = new TumblrApi(this);
 		List<Cookie> cookies = api.authenticateAndReturnCookies();
 
-		
 		if (cookies != null) {		
 			for (Cookie cookie : cookies) {
 				String cookieString = cookie.getName() + "=" + cookie.getValue() + "; domain=" + cookie.getDomain();
@@ -34,6 +33,17 @@ public class Dashboard extends Activity {
 			CookieSyncManager.getInstance().sync();
 		}
 		
+		web.getSettings().setJavaScriptEnabled(true);
+		web.setWebViewClient(new InsideWebViewClient());
 		web.loadUrl("http://www.tumblr.com/iphone"); // wish it was named mobile :)
 	}
+	
+	// from http://www.androidsnippets.org/snippets/26/
+	private class InsideWebViewClient extends WebViewClient {  
+        @Override  
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {  
+            view.loadUrl(url);  
+            return true;  
+        }  
+    }  
 }
