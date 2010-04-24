88d2a028ebfb7ddc9f8a8b11efac03503c4ddd7f
Fredrik Leijon <fredrik.leijon@gmail.com>
2010-04-24 14:49:34 +0200

    working dashboard

--- src/com/tacticalnuclearstrike/tttumblr/TumblrApi.java
+++ src/com/tacticalnuclearstrike/tttumblr/TumblrApi.java
@@ -11,6 +11,7 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.StringBody;
@@ -79,9 +80,9 @@ public class TumblrApi {
 		return false;
 	}
 	
-	public HttpResponse authenticateAndReturnResponse()
+	public List<Cookie> authenticateAndReturnCookies()
 	{
-		HttpClient httpclient = new DefaultHttpClient();
+		DefaultHttpClient httpclient = new DefaultHttpClient();
 		HttpPost httppost = new HttpPost("http://www.tumblr.com/login");
 
 		try {
@@ -89,12 +90,11 @@ public class TumblrApi {
 			nameValuePairs.add(new BasicNameValuePair("email", getUserName()));
 			nameValuePairs
 					.add(new BasicNameValuePair("password", getPassword()));
-			nameValuePairs.add(new BasicNameValuePair("destination_url", "/iphone"));
 			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
-			HttpResponse response = httpclient.execute(httppost);
+			httpclient.execute(httppost);
 			
-			return response;
+			return httpclient.getCookieStore().getCookies();
 		} catch (ClientProtocolException e) {
 		} catch (IOException e) {
 		}
--- src/com/tacticalnuclearstrike/tttumblr/activites/Dashboard.java
+++ src/com/tacticalnuclearstrike/tttumblr/activites/Dashboard.java
@@ -1,11 +1,14 @@
 package com.tacticalnuclearstrike.tttumblr.activites;
 
+import java.util.List;
+
 import org.apache.http.Header;
-import org.apache.http.HttpResponse;
+import org.apache.http.cookie.Cookie;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
 import android.webkit.WebView;
 
 import com.tacticalnuclearstrike.tttumblr.TumblrApi;
@@ -14,22 +17,23 @@ public class Dashboard extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		CookieSyncManager.createInstance(this);
+		
 		WebView web = new WebView(this);
 		setContentView(web);
 
 		TumblrApi api = new TumblrApi(this);
-		HttpResponse responseWithCookies = api.authenticateAndReturnResponse();
+		List<Cookie> cookies = api.authenticateAndReturnCookies();
 
-		if (responseWithCookies != null) {
-			Header[] header = responseWithCookies.getHeaders("Set-Cookie");
-			String val = header[0].getValue();
-			for (Header head : header) {
-				CookieManager.getInstance().setCookie("http://www.tumblr.com",
-						head.getValue());
+		
+		if (cookies != null) {		
+			for (Cookie cookie : cookies) {
+				String cookieString = cookie.getName() + "=" + cookie.getValue() + "; domain=" + cookie.getDomain();
+				CookieManager.getInstance().setCookie(cookie.getDomain(), cookieString);
 			}
+			CookieSyncManager.getInstance().sync();
 		}
 		
-		web.loadUrl("http://tumblr.com/iphone"); // wish it was named mobile :)
+		web.loadUrl("http://www.tumblr.com/iphone"); // wish it was named mobile :)
 	}
 }
