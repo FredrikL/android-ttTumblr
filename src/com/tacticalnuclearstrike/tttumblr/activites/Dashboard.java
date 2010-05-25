3cce92c8c2d5459a93784b35478d242434ae4e69
Fredrik Leijon <fredrik.leijon@gmail.com>
2010-05-25 18:49:38 +0200

    added progressdialog on dashboard loading

--- src/com/tacticalnuclearstrike/tttumblr/activites/Dashboard.java
+++ src/com/tacticalnuclearstrike/tttumblr/activites/Dashboard.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.http.cookie.Cookie;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.webkit.CookieManager;
@@ -16,20 +17,30 @@ import com.tacticalnuclearstrike.tttumblr.TumblrApi;
 
 public class Dashboard extends Activity {
 	private WebView webView;
-	
+	private ProgressDialog pd;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		CookieSyncManager.createInstance(this);
+		pd = ProgressDialog.show(this, "Loading..",
+				"Loading dashboard, please wait", true, false);
 
 		webView = new WebView(this);
-		setContentView(webView);
 
-		setupCookies();
+		new Thread(new Runnable() {
+			public void run() {
 
-		webView.getSettings().setJavaScriptEnabled(true);
-		webView.setWebViewClient(new InsideWebViewClient());
-		webView.loadUrl("http://www.tumblr.com/iphone");
+				setContentView(webView);
+
+				setupCookies();
+
+				webView.getSettings().setJavaScriptEnabled(true);
+				webView.setWebViewClient(new InsideWebViewClient());
+				webView.loadUrl("http://www.tumblr.com/iphone");
+			}
+		}).start();
 	}
 
 	private void setupCookies() {
@@ -50,20 +61,26 @@ public class Dashboard extends Activity {
 	private class InsideWebViewClient extends WebViewClient {
 		@Override
 		public boolean shouldOverrideUrlLoading(WebView view, String url) {
-			if(url.toLowerCase().contains("tumblr.com")){
+			if (url.toLowerCase().contains("tumblr.com")) {
 				view.loadUrl(url);
 				return true;
 			}
 			return false;
 		}
+		
+		@Override
+		public void onPageFinished(WebView view, String url)
+		{
+			pd.dismiss();
+		}
 	}
-	
+
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
-	    if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
-	        webView.goBack();
-	        return true;
-	    }
-	    return super.onKeyDown(keyCode, event);
+		if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+			webView.goBack();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
 	}
 }
