7098eae6d3db4e4aecbc41d40e1f321fb4c43510
Steve Pomeroy <steve@staticfree.info>
2010-06-28 17:40:50 -0400

    Added share intent handling for text, links and images.
    From the gallery, you can now share an image to ttTumblr.
    The same goes for URLs from the browser, or emails from
    k9.

--- src/com/tacticalnuclearstrike/tttumblr/activites/PostLinkActivity.java
+++ src/com/tacticalnuclearstrike/tttumblr/activites/PostLinkActivity.java
@@ -1,6 +1,7 @@
 package com.tacticalnuclearstrike.tttumblr.activites;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +18,12 @@ public class PostLinkActivity extends Activity {
 		setContentView(R.layout.postlinkview);
 
 		setupOkButton();
+
+		Intent intent = getIntent();
+		String action = intent.getAction();
+		if (Intent.ACTION_SEND.equals(action)){
+			((EditText)findViewById(R.id.inputUrl)).setText(intent.getExtras().getString(Intent.EXTRA_TEXT));
+		}
 	}
 	
 	private void returnToMainActivity() {
