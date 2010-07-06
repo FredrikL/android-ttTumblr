88d42ad9abb0fc8500ae3481add26b07ffcac7e5
Marc Dougherty <muncus@gmail.com>
2010-07-05 18:23:18 -0700

    store the list of available blogs, and allow for the use of 'options' passed to a new post. post options only supported on text and image so far.

--- src/com/tacticalnuclearstrike/tttumblr/TumblrApi.java
+++ src/com/tacticalnuclearstrike/tttumblr/TumblrApi.java
@@ -21,6 +21,10 @@ import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+import org.xmlpull.v1.XmlPullParserException;
+
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -29,12 +33,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.Bundle;
 import android.util.Log;
 import android.webkit.MimeTypeMap;
 
 import org.nerdcircus.android.tumblr.MediaUriBody;
 
 public class TumblrApi {
+    public static final String TAG = "TumblrApi";
+    public static final String BLOGS_PREFS = "blogs";
+
+    public static final String GENERATOR = "ttTumblr"; //user-agent string.
+
 	private Context context;
 
 	public TumblrApi(Context context) {
@@ -80,13 +90,15 @@ public class TumblrApi {
 			if (response.getStatusLine().getStatusCode() != 200) {
 				return false;
 			}
+            //Save our list of available blogs.
+            SharedPreferences blogs = context.getSharedPreferences(BLOGS_PREFS, 0);
+            saveBlogList(response, blogs);
 			return true;
 		} catch (ClientProtocolException e) {
 		} catch (IOException e) {
 		}
-
-		return false;
-	}
+        return false;
+    }
 
 	public List<Cookie> authenticateAndReturnCookies() {
 		DefaultHttpClient httpclient = new DefaultHttpClient();
@@ -116,19 +128,89 @@ public class TumblrApi {
 			entity.addPart("password", new StringBody(getPassword()));
 			if (Private)
 				entity.addPart("private", new StringBody("1"));
-			entity.addPart("generator", new StringBody("ttTumblr"));
+			entity.addPart("generator", new StringBody(GENERATOR));
 
+            /*
 			if (getIntegrateWithTwitter()) {
 				entity.addPart("send-to-twitter", new StringBody("auto"));
 			} else {
 				entity.addPart("send-to-twitter", new StringBody("no"));
 			}
+            */
 
 		} catch (UnsupportedEncodingException e) {
 			Log.e("ttTumblr", e.getMessage());
 		}
 		return entity;
 	}
+
+    /* post options are fields that can be set in the tumblr api:
+     * - send-to-twitter 
+     * - private
+     * - group
+     * - tags
+     * - format
+     * etc. 
+     */
+	public MultipartEntity getEntityWithOptions(Bundle options) {
+        MultipartEntity entity = getEntityWithBaseParamsSet(false);
+        if (options == null){ return entity; };
+        try {
+            //TODO: detect if the options have already been set?
+            //FIXME: use a foreach loop here.
+            if( options.containsKey("send-to-twitter")){
+                entity.addPart("send-to-twitter", new StringBody(options.getString("send-to-twitter")));
+                Log.d(TAG, "send-to-twitter: " + options.getString("send-to-twitter"));
+            }
+            if( options.containsKey("group")){
+                entity.addPart("group", new StringBody(options.getString("group")));
+                Log.d(TAG, "group: " + options.getString("group"));
+            }
+            if( options.containsKey("private")){
+                entity.addPart("private", new StringBody(options.getString("private")));
+                Log.d(TAG, "private: " + options.getString("private"));
+            }
+		} catch (UnsupportedEncodingException e) {
+			Log.e(TAG, "unsupported encoding: " + e.getMessage());
+		}
+        return entity;
+    }
+
+    /* helper to enclose all the http-related code */
+    private HttpResponse postEntity(MultipartEntity entity){
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httppost = new HttpPost("http://www.tumblr.com/api/write");
+		try {
+			httppost.setEntity(entity);
+			HttpResponse response = httpclient.execute(httppost);
+            return response;
+		} catch (ClientProtocolException e) {
+            Log.e(TAG, "client proto exception!", e);
+		} catch (IOException e) {
+            Log.e(TAG, "io exception", e);
+		}
+		return null;
+    }
+
+	public boolean postText(String title, String body, Bundle options) {
+        MultipartEntity entity = getEntityWithOptions(options);
+        try {
+            entity.addPart("type", new StringBody("regular"));
+            if(title != null)
+                entity.addPart("title", new StringBody(title));
+            if(body != null)
+                entity.addPart("body", new StringBody(body));
+        }
+        catch(UnsupportedEncodingException e){
+            Log.e(TAG, e.getMessage());
+        }
+        HttpResponse response = postEntity(entity);
+        if (response.getStatusLine().getStatusCode() != 201) {
+            ShowNotification("ttTumblr", "Text creation failed", "");
+            return false;
+        }
+        return true;
+    }
 
 	public boolean postText(String Title, String Body, Boolean Private) {
 		HttpClient httpclient = new DefaultHttpClient();
@@ -156,6 +238,34 @@ public class TumblrApi {
 	}
 
 
+	public boolean postImage(Uri image, String caption, Bundle options) {
+        MultipartEntity entity = getEntityWithOptions(options);
+        try {
+            entity.addPart("type", new StringBody("photo"));
+            if(caption != null)
+                entity.addPart("caption", new StringBody(caption));
+            // Do the MediaUriBody dance.
+            // Getting the type of the file
+            ContentResolver cR = context.getContentResolver();
+            MimeTypeMap mime = MimeTypeMap.getSingleton();
+            String type = mime.getExtensionFromMimeType(cR.getType(image));
+            //TODO: fix filename to be something unique.
+            InputStream stream = cR.openInputStream(image);
+            entity.addPart("data", new MediaUriBody(context, image, stream, "file."+type));
+
+        } catch (FileNotFoundException e) {
+            //from MediaUriBody constructor.
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            Log.d(TAG, e.getMessage());
+        }
+        HttpResponse response = postEntity(entity);
+        if (response.getStatusLine().getStatusCode() != 201) {
+            ShowNotification("ttTumblr", "Text creation failed", "");
+            return false;
+        }
+        return true;
+    }
 	public void PostImage(Uri image, String caption) {
 		HttpClient httpclient = new DefaultHttpClient();
 		HttpPost httppost = new HttpPost("http://www.tumblr.com/api/write");
@@ -351,4 +461,29 @@ public class TumblrApi {
 
 		return true;
 	}
+
+    private void saveBlogList(HttpResponse r, SharedPreferences bloglist){
+        try{
+            XmlPullParser xpp = XmlPullParserFactory.newInstance().newPullParser();
+            xpp.setInput(r.getEntity().getContent(), null);
+            int eventType = xpp.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                if(eventType == XmlPullParser.START_TAG && xpp.getName().equals("tumblelog")){
+                    String title = xpp.getAttributeValue(null, "title");
+                    String type = xpp.getAttributeValue(null, "type");
+                    if(type.equals("public")){
+                        String name = xpp.getAttributeValue(null, "name");  
+                        Log.d(TAG, "found public blog named: " + name);
+                        bloglist.edit().putString(title, name).commit();
+                    }
+                }
+                xpp.next();
+            }
+        } catch (XmlPullParserException e){
+            Log.e(TAG, "blog list parser failure", e);
+        } catch (IOException e){
+            Log.e(TAG, "i/o error", e);
+        }
+
+    }
 }
