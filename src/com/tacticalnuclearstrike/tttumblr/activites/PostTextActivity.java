cbea6c42e7216971b322d39cc8ec49fe7840fc82
Marc Dougherty <muncus@gmail.com>
2010-07-05 20:02:31 -0700

    blog select proof of concept

--- src/com/tacticalnuclearstrike/tttumblr/activites/PostTextActivity.java
+++ src/com/tacticalnuclearstrike/tttumblr/activites/PostTextActivity.java
@@ -3,6 +3,9 @@ package com.tacticalnuclearstrike.tttumblr.activites;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,12 +15,17 @@ import com.tacticalnuclearstrike.tttumblr.R;
 import com.tacticalnuclearstrike.tttumblr.TumblrApi;
 
 public class PostTextActivity extends Activity {
+
+    private Bundle mPostOptions = new Bundle();
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.posttextview);
 
 		setupOkButton();
+
+        loadDefaultPostOptions();
 	}
 	
 	private void returnToMainActivity() {
@@ -56,4 +64,18 @@ public class PostTextActivity extends Activity {
 		
 		returnToMainActivity();
 	}
+
+    private void loadDefaultPostOptions(){
+        //TODO: fill this in.
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        SubMenu blogmenu = menu.addSubMenu(Menu.NONE, Menu.NONE, Menu.NONE, "Select Tumblelog");
+        blogmenu.add(Menu.NONE, Menu.NONE, Menu.NONE, "blog 1");
+        blogmenu.add(Menu.NONE, Menu.NONE, Menu.NONE, "blog 2");
+        blogmenu.setGroupCheckable(Menu.NONE, true, true); 
+
+        return true;
+    }
 }
