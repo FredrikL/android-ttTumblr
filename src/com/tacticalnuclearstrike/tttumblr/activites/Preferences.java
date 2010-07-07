8c04c9e4d6a28ef4384edd4d6e12e5ff88012c8b
Marc Dougherty <muncus@gmail.com>
2010-07-07 14:19:20 -0700

    POC "default blog" preference

--- src/com/tacticalnuclearstrike/tttumblr/activites/Preferences.java
+++ src/com/tacticalnuclearstrike/tttumblr/activites/Preferences.java
@@ -1,10 +1,14 @@
 package com.tacticalnuclearstrike.tttumblr.activites;
 
+import java.util.ArrayList;
+import java.lang.CharSequence;
+
 import android.content.SharedPreferences;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.os.Build;
@@ -12,6 +16,7 @@ import android.os.Build;
 import android.util.Log;
 
 import com.tacticalnuclearstrike.tttumblr.R;
+import com.tacticalnuclearstrike.tttumblr.TumblrApi;
 
 public class Preferences extends PreferenceActivity {
     
@@ -20,6 +25,20 @@ public class Preferences extends PreferenceActivity {
         super.onCreate(savedInstanceState);
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+
+        ListPreference bloglistpref = (ListPreference)this.findPreference("default_blog");
+        SharedPreferences bloglist = getSharedPreferences(TumblrApi.BLOGS_PREFS, 0);
+
+        bloglistpref.setEnabled(true);
+        ArrayList<String> entries = new ArrayList();
+        ArrayList<String> entryVals = new ArrayList();
+
+        for (String k : bloglist.getAll().keySet()){
+            entries.add(k);
+            entryVals.add(bloglist.getString(k,""));
+        }
+        bloglistpref.setEntries(entries.toArray(new CharSequence[1]));
+        bloglistpref.setEntryValues(entryVals.toArray(new CharSequence[1]));
 
     }
 
