2314bceb5ebd54deee72b290ce8259c3e1739106
Fredrik Leijon <fredrik.leijon@gmail.com>
2010-07-21 19:49:08 +0200

    Service compatible with 1.6 android (and hopfully 1.5)

--- src/com/tacticalnuclearstrike/tttumblr/TumblrService.java
+++ src/com/tacticalnuclearstrike/tttumblr/TumblrService.java
@@ -1,6 +1,10 @@
 package com.tacticalnuclearstrike.tttumblr;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
@@ -33,14 +37,97 @@ public class TumblrService extends Service {
     @Override
     public void onCreate() {
         Log.d(TAG, "oncreate!");
+        mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        try {
+            mStartForeground = getClass().getMethod("startForeground",
+                    mStartForegroundSignature);
+            mStopForeground = getClass().getMethod("stopForeground",
+                    mStopForegroundSignature);
+        } catch (NoSuchMethodException e) {
+            // Running on an older platform.
+            mStartForeground = mStopForeground = null;
+        }
+    }
+    
+    private static final Class[] mStartForegroundSignature = new Class[] {
+        int.class, Notification.class};
+    private static final Class[] mStopForegroundSignature = new Class[] {
+        boolean.class};
+
+    private NotificationManager mNM;
+    private Method mStartForeground;
+    private Method mStopForeground;
+    private Object[] mStartForegroundArgs = new Object[2];
+    private Object[] mStopForegroundArgs = new Object[1];
+
+    /**
+     * This is a wrapper around the new startForeground method, using the older
+     * APIs if it is not available.
+     */
+    void startForegroundCompat(int id, Notification notification) {
+        // If we have the new startForeground API, then use it.
+        if (mStartForeground != null) {
+            mStartForegroundArgs[0] = Integer.valueOf(id);
+            mStartForegroundArgs[1] = notification;
+            try {
+                mStartForeground.invoke(this, mStartForegroundArgs);
+            } catch (InvocationTargetException e) {
+                // Should not happen.
+                Log.w("ApiDemos", "Unable to invoke startForeground", e);
+            } catch (IllegalAccessException e) {
+                // Should not happen.
+                Log.w("ApiDemos", "Unable to invoke startForeground", e);
+            }
+            return;
+        }
+
+        // Fall back on the old API.
+        setForeground(true);
+        mNM.notify(id, notification);
+    }
+
+    /**
+     * This is a wrapper around the new stopForeground method, using the older
+     * APIs if it is not available.
+     */
+    void stopForegroundCompat(Boolean value) {
+        // If we have the new stopForeground API, then use it.
+        if (mStopForeground != null) {
+            mStopForegroundArgs[0] = Boolean.TRUE;
+            try {
+                mStopForeground.invoke(this, mStopForegroundArgs);
+            } catch (InvocationTargetException e) {
+                // Should not happen.
+                Log.w("ApiDemos", "Unable to invoke stopForeground", e);
+            } catch (IllegalAccessException e) {
+                // Should not happen.
+                Log.w("ApiDemos", "Unable to invoke stopForeground", e);
+            }
+            return;
+        }
+
+        // Fall back on the old API.  Note to cancel BEFORE changing the
+        // foreground state, since we could be killed at that point.
+        //mNM.cancel(id);
+        setForeground(false);
     }
 
     @Override
     public IBinder onBind(Intent i){return null;} // dont use onBind.
+    
+    @Override
+    public void onStart(Intent intent, int startId) {
+        handleCommand(intent);
+    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d(TAG, "start intent received: " + intent.getAction());
+        handleCommand(intent);
+        return START_REDELIVER_INTENT;
+    }
+
+	private void handleCommand(Intent intent) {
+		Log.d(TAG, "start intent received: " + intent.getAction());
         if (ACTION_POST_TEXT.equals(intent.getAction())){
             doTextPost(intent);
         } else if (ACTION_POST_PHOTO.equals(intent.getAction())) {
@@ -55,8 +142,7 @@ public class TumblrService extends Service {
         else {
             Log.d(TAG, "UNKNOWN ACTION!");
         }
-        return START_REDELIVER_INTENT;
-    }
+	}
 
     //TODO: should these posts be cached somewhere so we can retry?
     private void doTextPost(Intent i){
@@ -67,10 +153,10 @@ public class TumblrService extends Service {
         Log.d(TAG, "attempting text post..");
 		new Thread(new Runnable() {
 			public void run() {
-                startForeground(N_POSTING, getNotification("test"));
+				startForegroundCompat(N_POSTING, getNotification("text"));
                 Log.d(TAG, "calling api.");
 				api.postText(titleText, postText, options);
-                stopForeground(true);
+				stopForegroundCompat(true);
 			}
 		}).start();
     }
@@ -85,9 +171,9 @@ public class TumblrService extends Service {
 		final TumblrApi api = new TumblrApi(this);
 		new Thread(new Runnable() {
 			public void run() {
-                startForeground(N_POSTING, getNotification("photo"));
+				startForegroundCompat(N_POSTING, getNotification("photo"));
 				api.postImage(photo, text, options);
-                stopForeground(true);
+				stopForegroundCompat(true);
 			}
 		}).start();
     }
@@ -102,9 +188,9 @@ public class TumblrService extends Service {
 		final TumblrApi api = new TumblrApi(this);
 		new Thread(new Runnable() {
 			public void run() {
-                startForeground(N_POSTING, getNotification("conversation"));
+				startForegroundCompat(N_POSTING, getNotification("conversation"));
 				api.postConversation(title, convo, options);
-                stopForeground(true);
+				stopForegroundCompat(true);
 			}
 		}).start();
     }
@@ -119,9 +205,9 @@ public class TumblrService extends Service {
 		final TumblrApi api = new TumblrApi(this);
 		new Thread(new Runnable() {
 			public void run() {
-                startForeground(N_POSTING, getNotification("quote"));
+				startForegroundCompat(N_POSTING, getNotification("quote"));
 				api.postQuote(quote, source, options);
-                stopForeground(true);
+				stopForegroundCompat(true);
 			}
 		}).start();
     }
@@ -137,9 +223,9 @@ public class TumblrService extends Service {
 		final TumblrApi api = new TumblrApi(this);
 		new Thread(new Runnable() {
 			public void run() {
-                startForeground(N_POSTING, getNotification("url"));
+				startForegroundCompat(N_POSTING, getNotification("url"));
 				api.postUrl(link, name, description, options);
-                stopForeground(true);
+				stopForegroundCompat(true);
 			}
 		}).start();
     }
@@ -157,5 +243,6 @@ public class TumblrService extends Service {
     public void onDestroy() {
         // Tell the user we stopped.
         Toast.makeText(this, "tumblr service stopped!", Toast.LENGTH_SHORT).show();
+        stopForegroundCompat(true);
     }
 }
